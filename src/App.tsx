02579ed5078e794e1b7f32327c06b86c2ababd26
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2025-03-15 11:25:25 -0300

    feat: restructure App component layout and update theme styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { Stack, ThemeProvider, Typography } from "@mui/material";
+import { Box, CssBaseline, ThemeProvider } from "@mui/material";
+import { Form } from "./components/Form";
+import { Header } from "./components/Header";
 import { theme } from "./theme";
 
 export function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Stack spacing={4} alignItems={"center"}>
-        <Typography color="textPrimary">Agrotis</Typography>
-      </Stack>
+      <CssBaseline />
+      <Box height="100vh" display="flex" flexDirection="column">
+        <Header />
+        <Box flex={1} p="2rem 1rem" overflow="auto">
+          <Form />
+        </Box>
+      </Box>
     </ThemeProvider>
   );
 }
--- src/theme.ts
+++ src/theme.ts
@@ -10,15 +10,45 @@ export const theme = createTheme({
       main: "#f4a62e",
     },
     background: {
-      default: "#f4f4f4",
+      default: "#eff3f3",
       paper: "#ffffff",
     },
     text: {
       primary: "#2a3b46",
-      secondary: "#ffffff",
     },
   },
   typography: {
     fontFamily: "'Roboto', sans-serif",
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        html: {
+          boxSizing: "border-box",
+        },
+        body: {
+          margin: 0,
+          padding: 0,
+          border: 0,
+        },
+        img: {
+          maxWidth: "100%",
+          height: "auto",
+        },
+      },
+    },
+    MuiFilledInput: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#ffffff",
+          "&:hover": {
+            backgroundColor: "#ffffff",
+          },
+          "&.Mui-focused": {
+            backgroundColor: "#ffffff",
+          },
+        },
+      },
+    },
   },
 });
