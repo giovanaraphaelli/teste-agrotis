fa766af6e736eb4deabf629f87d7c59e1c11a99c
Giovana Raphaelli <giovanaraphaelli@outlook.com>
2025-03-15 13:37:49 -0300

    feat: add Form component with validation schema and input fields

---
+++ src/components/Form/form-schema.ts
@@ -0,0 +1,34 @@
+import { z } from "zod";
+
+const dateSchema = z.coerce
+  .date({
+    errorMap: () => {
+      return {
+        message: "Campo obrigatório",
+      };
+    },
+  })
+  .min(new Date("1900-01-01"), { message: "Data muito antiga." });
+
+export const formSchema = z.object({
+  nome: z.string().min(1, "Campo obrigatório"),
+  data: z
+    .object({
+      dataInicial: dateSchema,
+      dataFinal: dateSchema,
+    })
+    .refine(
+      (data) => {
+        return data.dataInicial <= data.dataFinal;
+      },
+      {
+        message: "Data final deve ser maior que a data inicial.",
+        path: ["dataFinal"],
+      }
+    ),
+  propriedade: z.string().min(1, "Campo obrigatório"),
+  laboratorio: z.string().min(1, "Campo obrigatório"),
+  observacoes: z.string().optional(),
+});
+
+export type DefaultForm = z.infer<typeof formSchema>;
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,27 +1,162 @@
+import { zodResolver } from "@hookform/resolvers/zod";
 import {
   AppBar,
   Box,
+  Button,
   Card,
   CardContent,
+  FormControl,
+  Grid2,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
   Toolbar,
   Typography,
 } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { DefaultForm, formSchema } from "./form-schema";
 
 export function Form() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (data: DefaultForm) => {
+    console.log(data);
+  };
+
   return (
-    <Box maxWidth="md" margin="auto">
+    <Box maxWidth="lg" margin="auto">
       <Card variant="outlined">
         <AppBar position="static" elevation={0}>
-          <Toolbar>
+          <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
             <Typography variant="h1" fontSize={18}>
               Teste front-end
             </Typography>
+            <Button
+              type="submit"
+              sx={{ color: "#fff" }}
+              onClick={handleSubmit(onSubmit)}
+            >
+              Salvar
+            </Button>
           </Toolbar>
         </AppBar>
+
         <CardContent>
-          <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
-            AGROTIS
-          </Box>
+          <Grid2 container spacing={2}>
+            <Grid2 size={6}>
+              <TextField
+                id="nome"
+                label="Nome"
+                required
+                fullWidth
+                variant="filled"
+                {...register("nome")}
+                error={!!errors.nome}
+                helperText={errors.nome?.message}
+              />
+            </Grid2>
+
+            <Grid2 size={3}>
+              <TextField
+                id="dataInicial"
+                label="Data Inicial"
+                type="date"
+                required
+                fullWidth
+                variant="filled"
+                {...register("data.dataInicial")}
+                error={!!errors.data?.dataInicial}
+                helperText={errors.data?.dataInicial?.message}
+                slotProps={{
+                  inputLabel: {
+                    shrink: true,
+                  },
+                }}
+              />
+            </Grid2>
+
+            <Grid2 size={3}>
+              <TextField
+                id="dataFinal"
+                {...register("data.dataFinal")}
+                label="Data Final"
+                type="date"
+                required
+                fullWidth
+                variant="filled"
+                error={!!errors.data?.dataFinal}
+                helperText={errors.data?.dataFinal?.message}
+                slotProps={{
+                  inputLabel: {
+                    shrink: true,
+                  },
+                }}
+              />
+            </Grid2>
+
+            <Grid2 size={6}>
+              <FormControl fullWidth>
+                <InputLabel id="propriedade">Propriedades *</InputLabel>
+                <Select
+                  label="Propriedade"
+                  variant="filled"
+                  {...register("propriedade")}
+                  error={!!errors.propriedade}
+                  defaultValue={"" as any}
+                >
+                  <MenuItem value="1">1</MenuItem>
+                  <MenuItem value="2">2</MenuItem>
+                </Select>
+                {errors.propriedade && (
+                  <Typography variant="caption" color="error" marginLeft={2}>
+                    {errors.propriedade.message}
+                  </Typography>
+                )}
+              </FormControl>
+            </Grid2>
+
+            <Grid2 size={6}>
+              <FormControl fullWidth>
+                <InputLabel id="laboratorio">Laboratório *</InputLabel>
+                <Select
+                  label="Laboratório"
+                  variant="filled"
+                  {...register("laboratorio")}
+                  error={!!errors.laboratorio}
+                  defaultValue={"" as any}
+                >
+                  <MenuItem value="1">1</MenuItem>
+                  <MenuItem value="2">2</MenuItem>
+                </Select>
+                {errors.laboratorio && (
+                  <Typography variant="caption" color="error" marginLeft={2}>
+                    {errors.laboratorio.message}
+                  </Typography>
+                )}
+              </FormControl>
+            </Grid2>
+
+            <Grid2 size={12}>
+              <TextField
+                id="observacoes"
+                {...register("observacoes")}
+                label="Observações"
+                multiline
+                minRows={3}
+                fullWidth
+                variant="filled"
+                error={!!errors.observacoes}
+                helperText={errors.observacoes?.message}
+              />
+            </Grid2>
+          </Grid2>
         </CardContent>
       </Card>
     </Box>
